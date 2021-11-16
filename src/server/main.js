3244aa3927eab4919a207e8ba9154e8f7b8aec6a
username <greenrock46@gmail.com>
2021-11-17 02:09:14 +0900

    코드 정리

--- src/server/main.js
+++ src/server/main.js
@@ -1,15 +1,13 @@
 const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
-const {Server} = require('socket.io')
+const {Server} = require('socket.io');
 const io = new Server(server);
 const path = require('path');
 const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended: true}));
 
-server.listen(3000, ()=>{
-    console.log('3000 port ON!');
-})
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static(path.join(__dirname, "../../build")));
 
@@ -35,20 +33,20 @@ class PlayerBall{
     }
 }
 
-var balls = [];
-var ballMap = {};
+let balls = [];
+let ballMap = {};
 
 function joinGame(socket){
     let ball = new PlayerBall(socket);
-
+    
     balls.push(ball);
     ballMap[socket.id] = ball;
-
+    
     return ball;
 }
 
 function endGame(socket){
-    for(var i=0; i<balls.length; i++){
+    for(let i=0; i<balls.length; i++){
         if(balls[i].id === socket.id){
             balls.splice(i,1);
             break
@@ -60,18 +58,18 @@ function endGame(socket){
 //client 연결시 아래 코드 안에서 통신함
 io.on('connection', (socket)=>{
     console.log(`${socket.id} is entered ${Date()}`);
-
+    
     //연결 종료시 작업
     socket.on('disconnect', (reason)=>{
         console.log(socket.id + ' has left because of ' + reason + ' ' + Date());
         endGame(socket);
-        socket.emit('leave_user', socket.id); //떠날 때 socket.id 값 송신
+        socket.broadcast.emit('leave_user', socket.id); //떠날 때 socket.id 값 송신
     })
-
+    
     //게임에 필요한 ball생성 작업
     let newBall = joinGame(socket);
     socket.emit('user_id', socket.id); //접속한 socket.id 송신
-
+    
     //생성된 ball들의 기초 정보 전송
     for(var i=0; i < balls.length; i++){
         let ball = balls[i];
@@ -82,14 +80,25 @@ io.on('connection', (socket)=>{
             color: ballColor[i],
         });
     }
-
+    socket.broadcast.emit('join_user', {
+        id: newBall.id,
+        x: newBall.x,
+        y: newBall.y,
+        color: newBall.color
+    });
+    
     //업데이트된 위치 정보 받아서
     socket.on('send_location', data =>{
         //각 클라이언트로 위치 정보 전송
-        socket.emit('update_state',{
+        socket.broadcast.emit('update_state',{
             id: data.id,
             x: data.x,
             y: data.y,
         })
     });
+})
+
+
+server.listen(3000, ()=>{
+    console.log('3000 port ON!');
 })
