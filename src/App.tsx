b32cfed6ae8f875da50fda81e6155533036e7d38
logan <rk172172@gmail.com>
2021-11-16 22:27:51 +0900

    미약한 발전

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,7 @@ import { Joystick } from 'react-joystick-component';
 import isWallCollision from './isWallCollision';
 import io from 'socket.io-client'
 
-/* ================== 서버 관련 시작 ================== */
-const socket = io('http://localhost:3000');
 
-socket.on('join_user', function(data){
-  gameData.x = data.x;
-  gameData.y = data.y;
-})
-/* ================== 서버 관련 끝 ================== */
 
 /* ================== 조이스틱 관련 시작 ================== */
 type JoystickDirection = "FORWARD" | "RIGHT" | "LEFT" | "BACKWARD";
@@ -26,59 +19,60 @@ interface IJoystickUpdateEvent {
 const handleMove = (event: IJoystickUpdateEvent) => {
   const x: number | null = event.x;
   const y: number | null = event.y;
-  gameData.state = event.type;
+  joystickData.state = event.type;
 
   if (x != null && y != null) {
-    gameData.moveX = x / 50;
-    gameData.moveY = y / -50;
+    joystickData.moveX = x / 50;
+    joystickData.moveY = y / -50;
   }
 }
 
 const handleStop = (event: IJoystickUpdateEvent) => {
-  gameData.state = event.type;
+  joystickData.state = event.type;
 }
 
 /* ================== 조이스틱 관련 끝 ================== */
 
-/* ================== 게임 정보 관련 시작 ================== */
-//Note: 현재 픽셀 위치 설정은 canvas 360x500을 기준으로 맞춰져있습니다.
-const CanvasWidth = 360;
-const CanvasHeight = 500;
-
-const ballRad = 20;
-const balls = [];
-const ballMap = {};
-// const myId;
-
-type playerBallType = {
-  id: any;
-  color: string;
-  x: number;
-  y: number;
-}
-
 class playerBall {
-  id: any;
+  id: string;
   color: string;
   x: number;
   y: number;
 
   constructor() {
-    this.color = "#FF00FF"
+    this.id = "";
+    this.color = "#FF00FF";
     this.x = 360/2;
     this.y = 500/2;
   }
 }
 
-const gameData = {
-  x: 100,
-  y: 100,
+type playerBallType = {
+  id: string;
+  color: string;
+  x: number;
+  y: number;
+}
+
+/* ================== 게임 정보 관련 시작 ================== */
+//Note: 현재 픽셀 위치 설정은 canvas 360x500을 기준으로 맞춰져있습니다.
+const CanvasWidth = 360;
+const CanvasHeight = 500;
+const ballRad = 20;
+
+const balls: playerBallType[] = [];
+const ballMap: Record<string, playerBall> = {};
+let myId: string;
+
+
+
+const joystickData = {
   moveX: 0,
   moveY: 0,
   state: "stop",
 }
 
-function joinUser(id: any,color: string,x: number,y: number){
+function joinUser(id: string, color: string, x: number, y: number){
   let ball = new playerBall();
   ball.id = id;
   ball.color = color;
@@ -86,13 +80,58 @@ function joinUser(id: any,color: string,x: number,y: number){
   ball.y = y;
 
   balls.push(ball);
-  // ballMap[id] = ball;
+  ballMap[id] = ball;
 
   return ball;
 }
 
+function updateState(id: string, x: number, y: number){
+  for(let i = 0 ; i < balls.length; i++){
+    if(balls[i].id === id) {
+      balls[i].x = x;
+      balls[i].y = y;
+      break;
+    }
+  }
+
+  let ball = ballMap[id];
+  if(!ball){
+      return;
+  }
+  ball.x = x;
+  ball.y = y;
+}
+
 /* ================== 게임 정보 관련 끝 ================== */
 
+/* ================== 서버 관련 시작 ================== */
+const socket = io('http://localhost:3000');
+
+socket.on('join_user', function(data){
+  console.log("join_user");
+  let temp = joinUser(data.id, data.color, data.x, data.y);
+  console.log(temp);
+  console.log(ballMap);
+  console.log(balls);
+})
+
+socket.on('update_state', function(data){
+  updateState(data.id, data.x, data.y);
+})
+
+function sendData() {
+  let curPlayer = ballMap[myId];
+  let data = {};
+  data = {
+      id : curPlayer.id,
+      x: curPlayer.x,
+      y: curPlayer.y,
+  };
+  if(data){
+      socket.emit("send_location", data);
+  }
+}
+/* ================== 서버 관련 끝 ================== */
 
 /* ================== 캔버스 출력 관련 시작 ================== */
 
@@ -100,13 +139,13 @@ function ClearCanvas(ctx: any, canvas: any) {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
 
-function drawCircle(ctx: any) {
-  ctx.beginPath();
-  ctx.arc(gameData.x, gameData.y, ballRad, 0, 2 * Math.PI);
-  ctx.fillStyle = "red";
-  ctx.fill();
-  ctx.stroke();
-}
+// function drawCircle(ctx: any) {
+//   ctx.fillStyle = "red";
+//   ctx.beginPath();
+//   ctx.arc(x, y, ballRad, 0, 2 * Math.PI);
+//   ctx.fill();
+//   ctx.stroke();
+// }
 
 /* ================== 캔버스 출력 관련 끝================== */
 
@@ -123,23 +162,35 @@ function App() {
       /*==== 캔버스 요소 조작 시작====*/
       ClearCanvas(ctx, canvas);
 
-      isWallCollision(gameData, canvas, ballRad);
-
-      if (gameData.state === "move"){
-        gameData.x += gameData.moveX;
-        gameData.y += gameData.moveY;
-      } else if (gameData.state === "stop"){
-        gameData.moveX = 0;
-        gameData.moveY = 0;
+      for (let i = 0; i < balls.length; i++) {
+        let ball = balls[i];
+        
+        ctx.fillStyle = ball.color;
+        ctx.beginPath();
+        ctx.arc(ball.x, ball.y, ballRad, 0, 2 * Math.PI);
+        ctx.fill();
+        ctx.stroke();
       }
 
-      drawCircle(ctx);
-
       /*==== 캔버스 요소 조작 끝====*/
+
+      let curPlayer = ballMap[myId];
+
+      if (joystickData.state === "move"){
+        curPlayer.x += joystickData.moveX;
+        curPlayer.y += joystickData.moveY;
+      } else if (joystickData.state === "stop"){
+        joystickData.moveX = 0;
+        joystickData.moveY = 0;
+      }
+
+      sendData();
 
       //canvas에 애니메이션이 작동하게 하는 함수. 
       requestAnimationFrame(render);
     };
+
+    // setInterval(render, 10);
     render();
   });
 
